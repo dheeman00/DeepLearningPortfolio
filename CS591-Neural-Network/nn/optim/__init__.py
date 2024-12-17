2e6776fce3ac8a5cac7b4d2149e4f283f13472df
Dheeman Saha <dsaha@unm.edu>
2024-12-17 00:22:43 -0700

    Create __init__.py

---
+++ CS591-Neural-Network/nn/optim/__init__.py
@@ -0,0 +1,12 @@
+from nn.optim.adam import *
+from nn.optim.gd import GD
+from nn.optim.momentum import *
+from nn.optim.newton import *
+
+__all__ = [
+    'GD',
+    'Momentum',
+    'Nesterov',
+    'Newton',
+    'Adam'
+]
