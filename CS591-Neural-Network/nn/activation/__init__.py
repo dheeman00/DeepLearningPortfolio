83b3bd686190bc1177f9bfb64778661add23532e
Dheeman Saha <dsaha@unm.edu>
2024-12-16 23:59:45 -0700

    Create __init__.py

---
+++ CS591-Neural-Network/nn/activation/__init__.py
@@ -0,0 +1,14 @@
+from nn.activation.linear import LinearActivation
+from nn.activation.relu import ReLU
+from nn.activation.sigmoid import Sigmoid
+from nn.activation.softmax import *
+from nn.activation.tanh import Tanh
+
+__all__ = [
+    'Sigmoid',
+    'ReLU',
+    'Tanh',
+    'LinearActivation',
+    'SoftMax',
+    'LogSoftmax'
+]
